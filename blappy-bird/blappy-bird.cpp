4d71b25c3b1a650682d21c0be811ca0428d93fdf
Nikolai Escondo <nescondo2@gmail.com>
2026-01-01 19:23:31 -0500

    initial commit

---
+++ blappy-bird/blappy-bird.cpp
@@ -0,0 +1,12 @@
+﻿// blappy-bird.cpp : Defines the entry point for the application.
+//
+
+#include "blappy-bird.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
