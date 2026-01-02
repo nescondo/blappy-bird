11f33d927251f1b23a560a0f7b05373b3e03ceae
Nikolai Escondo <nescondo2@gmail.com>
2026-01-01 20:12:07 -0500

    linked SDL3 libraries, header files, dll, initialized SDL and created window in project

--- blappy-bird/blappy-bird.cpp
+++ blappy-bird/blappy-bird.cpp
@@ -2,11 +2,35 @@
 //
 
 #include "blappy-bird.h"
+#include <SDL3/SDL.h>
 
 using namespace std;
 
+void initialize();
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+	initialize();
+
+	SDL_Quit();
 	return 0;
+}
+
+void initialize()
+{
+	// initialize SDL w/ video
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_LogError(2, "Error initializing window");
+	}
+	
+	// create the window
+	SDL_Window* window;
+	window = SDL_CreateWindow(
+		"Blappy Bird",
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
 }
